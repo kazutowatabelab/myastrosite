1b0b5a11c737b13f4154e53b4f2fd2c4129f3776
kazuto watabe <kazutowatabe@kazutonoMacBook-Pro.local>
2026-01-11 16:20:05 +0900

    idを使ってURLを確定させた

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'kazutoのゲーム開発ブログ';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mirai Anchorage Japan';
+export const SITE_DESCRIPTION = 'UnityとAIの可能性を探求する、未来への停泊所。';
